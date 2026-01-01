b1b7f5a275666fc6a4185a612cf4950274aae54e
Jamie Temple <jamie.c.temple@gmail.com>
2026-01-01 12:26:00 +0100

    feat: add fail2ban to vps

--- hosts/vps/services.nix
+++ hosts/vps/services.nix
@@ -12,9 +12,33 @@ _:
     };
   };
 
+  # ===============================================================
+  #       FAIL2BAN
+  # ===============================================================
   services.fail2ban = {
     enable = true;
     maxretry = 5;
     bantime = "24h";
+    bantime-increment = {
+      enable = true;
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h"; # 1 week
+      overalljails = true;
+    };
+    jails = {
+      sshd = {
+        enabled = true;
+        port = "22";
+        filter = "sshd";
+        logpath = "/var/log/auth.log";
+        maxretry = 3;
+      };
+    };
   };
+
+
+  # ===============================================================
+  #       WIREGUARD
+  # ===============================================================
+
 }
